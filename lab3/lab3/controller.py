c447ebe7bbd664842d32b22ab2cc07d211c300e4
leon123858 <a0970785699@gmail.com>
2022-10-11 22:15:46 +0800

    complete lab3
    complete lab3

--- lab3/lab3/controller.py
+++ lab3/lab3/controller.py
@@ -1,7 +1,23 @@
 from enum import Enum
+from carla import Vector3D, Location
+import numpy as np
 import csv
+import math
+
 
 WAYPOINTS = []
+
+
+def relative_location(car_location, car_rotation, point_location):
+    origin = car_location
+    forward = car_rotation.get_forward_vector()
+    right = car_rotation.get_right_vector()
+    up = car_rotation.get_up_vector()
+    disp = point_location - origin
+    x = np.dot([disp.x, disp.y, disp.z], [forward.x, forward.y, forward.z])
+    y = np.dot([disp.x, disp.y, disp.z], [right.x, right.y, right.z])
+    z = np.dot([disp.x, disp.y, disp.z], [up.x, up.y, up.z])
+    return Vector3D(x, y, z)
 
 
 def load_waypoints(path):
@@ -15,10 +31,35 @@ def step(my_car):
 
     global WAYPOINTS   # The waypoints are stored here
 
-    car_orientation = my_car.get_transform().rotation.yaw   # Car orientation
-    location = (my_car.get_location().x,
-                my_car.get_location().y)   # Car location
-
+    wheelbase = 1
+    now_velocity = my_car.get_velocity().length()
+    # get next distance and angle
+    if len(WAYPOINTS) == 0:
+        control = my_car.get_control()
+        control.brake = 1  # 0~1
+        my_car.apply_control(control)
+        return
+    next_point = WAYPOINTS.pop(0)
+    vector_car2point = relative_location(my_car.get_location(), my_car.get_transform(
+    ).rotation, Location(x=next_point[0], y=next_point[1], z=my_car.get_location().z))
+    wheel_vector = Vector3D(wheelbase, 0, 0)
+    distance = vector_car2point.length()
+    relate_vector = vector_car2point+wheel_vector
+    d2 = relate_vector.x**2 + relate_vector.y**2
+    steer_rad = math.atan(2 * wheelbase * relate_vector.y / d2)
+    steer_deg = math.degrees(steer_rad)
+    steer_deg = np.clip(steer_deg, -1, 1)
+    steer = steer_deg / 1
+    # print("distance", distance, "angle", angle, "velocity", now_velocity)
+    if distance >= 0.5:
+        WAYPOINTS.insert(0, next_point)
     # TODO
     # Apply control to the car. See the API here.
     # https://carla.readthedocs.io/en/latest/python_api/#carla.VehicleControl
+    control = my_car.get_control()
+    p = 0.01
+    i = 0.005
+    control.throttle = distance*p + (20-now_velocity)*i
+    control.steer = steer
+    control.brake = 0  # 0~1
+    my_car.apply_control(control)
