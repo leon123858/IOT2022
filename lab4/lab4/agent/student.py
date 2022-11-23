76141f6b649d83679d5bb6f78e77ef4add1b8935
leon123858 <a0970785699@gmail.com>
2022-11-23 20:29:33 +0800

    change comment

--- lab4/lab4/agent/student.py
+++ lab4/lab4/agent/student.py
@@ -39,78 +39,82 @@ class Camera_Imager(Imager):
         image = np.clip(self.image, a_min=0, a_max=1)
         return np.sum(image[math.floor(image.shape[0]/2):, :], axis=0)
 
-    def find_line_fit(self, nwindows=9, margin=100, minpix=50):
-        image = self.image
-        histogram = self.get_position_histogram()
-        # Create an output image to draw on and  visualize the result
-        out_img = np.dstack((image, image, image)) * 255
-        # Find the peak of the left and right halves of the histogram
-        # These will be the starting point for the left and right lines
-        midpoint = np.int(histogram.shape[0]/2)
-        leftx_base = np.argmax(histogram[:midpoint])
-        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    # def find_line_fit(self, nwindows=9, margin=100, minpix=50):
+    #     image = self.image
+    #     histogram = self.get_position_histogram()
+    #     # Create an output image to draw on and  visualize the result
+    #     out_img = np.dstack((image, image, image)) * 255
+    #     # Find the peak of the left and right halves of the histogram
+    #     # These will be the starting point for the left and right lines
+    #     midpoint = np.int(histogram.shape[0]/2)
+    #     leftx_base = np.argmax(histogram[:midpoint])
+    #     rightx_base = np.argmax(histogram[midpoint:]) + midpoint
 
-        # Set height of windows
-        window_height = np.int(image.shape[0]/nwindows)
-        # Identify the x and y positions of all nonzero pixels in the image
-        nonzero = image.nonzero()
-        nonzeroy = np.array(nonzero[0])
-        nonzerox = np.array(nonzero[1])
-        # Current positions to be updated for each window
-        leftx_current = leftx_base
-        rightx_current = rightx_base
-        # Create empty lists to receive left and right lane pixel indices
-        left_lane_inds = []
-        right_lane_inds = []
+    #     # Set height of windows
+    #     window_height = np.int(image.shape[0]/nwindows)
+    #     # Identify the x and y positions of all nonzero pixels in the image
+    #     nonzero = image.nonzero()
+    #     nonzeroy = np.array(nonzero[0])
+    #     nonzerox = np.array(nonzero[1])
+    #     # Current positions to be updated for each window
+    #     leftx_current = leftx_base
+    #     rightx_current = rightx_base
+    #     # Create empty lists to receive left and right lane pixel indices
+    #     left_lane_inds = []
+    #     right_lane_inds = []
 
-        # Step through the windows one by one
-        for window in range(nwindows):
-            # Identify window boundaries in x and y (and right and left)
-            win_y_low = image.shape[0] - (window+1)*window_height
-            win_y_high = image.shape[0] - window*window_height
-            win_xleft_low = leftx_current - margin
-            win_xleft_high = leftx_current + margin
-            win_xright_low = rightx_current - margin
-            win_xright_high = rightx_current + margin
-            # Draw the windows on the visualization image
-            cv.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high),
-                         (0, 255, 0), 2)
-            cv.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high),
-                         (0, 255, 0), 2)
-            # Identify the nonzero pixels in x and y within the window
-            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
-                              (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
-            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
-                               (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
-            # Append these indices to the lists
-            left_lane_inds.append(good_left_inds)
-            right_lane_inds.append(good_right_inds)
-            # If you found > minpix pixels, recenter next window on their mean position
-            if len(good_left_inds) > minpix:
-                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
-            if len(good_right_inds) > minpix:
-                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+    #     # Step through the windows one by one
+    #     for window in range(nwindows):
+    #         # Identify window boundaries in x and y (and right and left)
+    #         win_y_low = image.shape[0] - (window+1)*window_height
+    #         win_y_high = image.shape[0] - window*window_height
+    #         win_xleft_low = leftx_current - margin
+    #         win_xleft_high = leftx_current + margin
+    #         win_xright_low = rightx_current - margin
+    #         win_xright_high = rightx_current + margin
+    #         # Draw the windows on the visualization image
+    #         cv.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high),
+    #                      (0, 255, 0), 2)
+    #         cv.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high),
+    #                      (0, 255, 0), 2)
+    #         # Identify the nonzero pixels in x and y within the window
+    #         good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+    #                           (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+    #         good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+    #                            (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+    #         # Append these indices to the lists
+    #         left_lane_inds.append(good_left_inds)
+    #         right_lane_inds.append(good_right_inds)
+    #         # If you found > minpix pixels, recenter next window on their mean position
+    #         if len(good_left_inds) > minpix:
+    #             leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+    #         if len(good_right_inds) > minpix:
+    #             rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
 
-        # Concatenate the arrays of indices
-        left_lane_inds = np.concatenate(left_lane_inds)
-        right_lane_inds = np.concatenate(right_lane_inds)
+    #     # Concatenate the arrays of indices
+    #     left_lane_inds = np.concatenate(left_lane_inds)
+    #     right_lane_inds = np.concatenate(right_lane_inds)
 
-        # Extract left and right line pixel positions
-        leftx = nonzerox[left_lane_inds]
-        lefty = nonzeroy[left_lane_inds]
-        rightx = nonzerox[right_lane_inds]
-        righty = nonzeroy[right_lane_inds]
+    #     # Extract left and right line pixel positions
+    #     leftx = nonzerox[left_lane_inds]
+    #     lefty = nonzeroy[left_lane_inds]
+    #     rightx = nonzerox[right_lane_inds]
+    #     righty = nonzeroy[right_lane_inds]
 
-        # to plot
-        out_img[nonzeroy[left_lane_inds],
-                nonzerox[left_lane_inds]] = [255, 0, 0]
-        out_img[nonzeroy[right_lane_inds],
-                nonzerox[right_lane_inds]] = [0, 0, 255]
+    #     # to plot
+    #     out_img[nonzeroy[left_lane_inds],
+    #             nonzerox[left_lane_inds]] = [255, 0, 0]
+    #     out_img[nonzeroy[right_lane_inds],
+    #             nonzerox[right_lane_inds]] = [0, 0, 255]
 
-        # Fit a second order polynomial to each
-        left_fit = np.polyfit(lefty, leftx, 2)
-        right_fit = np.polyfit(righty, rightx, 2)
-        return left_fit, right_fit, out_img
+    #     # Fit a second order polynomial to each
+    #     left_fit = []
+    #     right_fit = []
+    #     if leftx != [] and lefty != []:
+    #         left_fit = np.polyfit(lefty, leftx, 2)
+    #     if rightx != [] and righty != []:
+    #         right_fit = np.polyfit(righty, rightx, 2)
+    #     return left_fit, right_fit, out_img
 
 
 class StudentAgent:
